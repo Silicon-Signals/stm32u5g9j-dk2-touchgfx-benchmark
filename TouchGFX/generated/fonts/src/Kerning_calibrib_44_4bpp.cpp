2861606019ac30f5c6a8dce612482ada0dbd4589
Ridhhi Kavar <riddhi.kavar@siliconsignals.io>
2025-12-02 18:37:56 +0530

    Remove config and generated dir from gitignore

---
+++ TouchGFX/generated/fonts/src/Kerning_calibrib_44_4bpp.cpp
@@ -0,0 +1,18 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_calibrib_44_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x0041, -1 },  // (First char = [0x0041, A], Second char = [0x002D, -], Kerning dist = -1)
+    { 0x004B, -1 },  // (First char = [0x004B, K], Second char = [0x002D, -], Kerning dist = -1)
+    { 0x0041, -2 },  // (First char = [0x0041, A], Second char = [0x003F, ?], Kerning dist = -2)
+    { 0x0044, -1 },  // (First char = [0x0044, D], Second char = [0x0041, A], Kerning dist = -1)
+    { 0x004A, -1 },  // (First char = [0x004A, J], Second char = [0x0041, A], Kerning dist = -1)
+    { 0x0041, -1 },  // (First char = [0x0041, A], Second char = [0x0047, G], Kerning dist = -1)
+    { 0x004B, -2 },  // (First char = [0x004B, K], Second char = [0x0047, G], Kerning dist = -2)
+    { 0x0041,  1 },  // (First char = [0x0041, A], Second char = [0x004A, J], Kerning dist = 1)
+    { 0x0044, -1 },  // (First char = [0x0044, D], Second char = [0x004A, J], Kerning dist = -1)
+    { 0x0041, -3 },  // (First char = [0x0041, A], Second char = [0x0054, T], Kerning dist = -3)
+    { 0x0044, -1 },  // (First char = [0x0044, D], Second char = [0x0054, T], Kerning dist = -1)
+    { 0x0041, -1 },  // (First char = [0x0041, A], Second char = [0x0055, U], Kerning dist = -1)
+    { 0x004B, -1 },  // (First char = [0x004B, K], Second char = [0x0055, U], Kerning dist = -1)
+};
